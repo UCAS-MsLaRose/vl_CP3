6ee77df6c84de41c88eebfd46e9ecf4893cb01a0
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-08-22 11:49:35 -0600

    basics notes

---
+++ Notes/basics.cpp
@@ -0,0 +1,19 @@
+// VL Variables, Data Types, Inputs and Outputs
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int students = 11;
+    double days = 5;
+    long years = 2554l;
+    const float pi = 3.14f; //float will compile fto double without f at the end
+    bool on = true;
+    char name = 'V';
+    cout << "Tell me your age: ";
+    int age;
+    cin >> age;
+    double divide = students/days;
+    cout << divide;
+    return 0;
+}
