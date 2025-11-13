059c4bfbd5635ae47a5df4a0d435dc5cc5686e4f
UCAS-MsLaRose <vienna.larose@ucas-edu.net>
2025-11-13 11:43:37 -0700

    que and stacks notes

---
+++ python_notes/queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.queue = []
+    
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def enqueue(self,item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.queue[0]
+        
+    def size(self):
+        return len(self.queue)
